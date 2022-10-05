33ff1d038f576de55dacfaa35bce0c062840a300
IhorFedchenko <if.ihorfedchenko@gmail.com>
2022-10-05 10:41:50 +0300

    add modal window 5.10.22 version

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ const elemShowHidde = (elem, event) => {
   elem.style.display = event;
 };
 
-// GET DATA FROM SERVER
+// GET / SEND DATA FROM / TO SERVER
 async function fetchData(url, method, data) {
   return await fetch(url, {
     method: `${method}`,
@@ -289,6 +289,36 @@ followUsBtn.addEventListener("click", () => {
   socBox.offsetHeight === 0
     ? elemShowHidde(socBox, "block")
     : elemShowHidde(socBox, "none");
+});
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+// MODAL WINDOW
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".btn--close-modal");
+const btnsOpenModal = document.querySelectorAll(".btn--show-modal");
+
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnsOpenModal.forEach((btn) => btn.addEventListener("click", openModal));
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
 });
 
 /////////////////////////////////////////////////////////////
